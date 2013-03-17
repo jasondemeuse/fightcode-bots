c0ab4be8805914898bc76cd39ba61c55a15911dc
Jason Demeuse <demeuseja@gmail.com>
2013-03-17 18:31:26 -0500

    first win

--- awesomeo.js
+++ awesomeo.js
@@ -9,9 +9,10 @@ var Robot = function(robot) {
 Robot.prototype.onIdle = function(ev) {
     var robot = ev.robot;
     robot.ahead(100);
-    robot.rotateCannon(360);
-    robot.turn(20);
-    robot.rotateCannon(360);
+    robot.rotateCannon(180);
+    robot.turn(80);
+    robot.ahead(90);
+    robot.rotateCannon(180);
 
 };
 
@@ -31,4 +32,12 @@ Robot.prototype.onHitByBullet = function(ev) {
 
 Robot.prototype.onRobotCollision = function(ev) {
     var robot = ev.robot;
+};
+
+Robot.prototype.onWallCollision = function(ev) {
+    var robot = ev.robot;
+    robot.back(100);
+    robot.rotate(100);
+    robot.fire()
+    robot.ahead(50);
 };
